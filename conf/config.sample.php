e3623247e296a80171ca41d2fd29dc361ec99f65
linostar <linux.anas@gmail.com>
2015-12-16 18:56:55 +0200

    Added config.sample.php

---
+++ conf/config.sample.php
@@ -0,0 +1,16 @@
+<?
+	// MySQL config for trivia-report db access
+	$conn_report_host = "localhost";
+	$conn_report_port = "3306";
+	$conn_report_db = "trivia_report_db";
+	$conn_report_user = "trivia_report_user";
+	$conn_report_pass = "foo";
+
+	// MySQL config for Trivia bot's db access (for retrieving trivia questions and answers)
+	$conn_trivia_host = "";
+	$conn_trivia_port = "";
+	$conn_trivia_db = "";
+	$conn_trivia_user = "";
+	$conn_trivia_pass = "";
+	$conn_trivia_table_prefix = "trivia_questions_";
+?>
