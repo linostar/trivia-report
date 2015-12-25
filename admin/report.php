a0fb35241b7276f2fb584883440e6a8a155b032e
linostar <linux.anas@gmail.com>
2015-12-25 16:09:54 +0200

    Moved common functions to utils.php

--- admin/report.php
+++ admin/report.php
@@ -24,81 +24,27 @@
 	$db->start();
 	$all_reasons = $db->get_all_reasons();
 
-		function display_admin_navbar() {
-?>
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" style="background-color: black;" href="index.php">Admin Panel</a>
-			</div>
-			<div class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="https://rizon.net/">Rizon Home</a></li>
-					<li><a href="../index.php">Trivia Reporter</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li ><a href="?action=logout">Logout</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-<?php
-	}
-
-		function display_login() {
-?>
-	<div id="login_container">
-		<div class="panel panel-primary panel_login">
-			<div class="panel-heading">
-				<div class="panel-title"><center>Admin Panel</center></div>
-			</div>
-			<div class="panel-body">
-				<form id="formLogin" action="index.php" method="post">
-					<div class="form-group">
-						<label for="txtUser" class="control-label">Username</label>
-						<input type="text" name="txtUser" id="txtUser" value="" class="form-control">
-					</div>
-					<div class="form-group">
-						<label for="txtPass" class="control-label">Password</label>
-						<input type="password" name="txtPass" id="txtPass" value="" class="form-control">
-					</div>
-					<div class="form-group">
-						<center><button type="submit" name="loginButton" class="btn btn-primary">Login</button></center>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-<?php
-	}
-
 	if ($_POST["txtUser"] && $_POST["txtPass"]) {
 		if (Utils::login($_POST["txtUser"], $_POST["txtPass"])) {
-			display_admin_navbar();
+			Utils::display_admin_navbar();
 			//display_report_page();
 		}
 		else {
 			echo '<center><div class="alert alert-danger panel_login">Wrong username or password!</div></center>';
-			display_login();
+			Utils::display_login();
 		}
 	}
 	else if ($_GET["action"] == "logout") {
 		Utils::logout();
 		echo '<center><div class="alert alert-info panel_login">You have been successfully logged out.</div></center>';
-		display_login();
+		Utils::display_login();
 	}
 	else if ($_SESSION["username"] && $_SESSION["password"]) {
-		display_admin_navbar();
+		Utils::display_admin_navbar();
 		//display_report_page();
 	}
 	else {
-		display_login();
+		Utils::display_login();
 	}
 
 	$db->stop();
--- admin/utils.php
+++ admin/utils.php
@@ -16,5 +16,59 @@
 		public static function logout() {
 			session_unset();
 		}
+
+		public static function display_login() {
+?>
+	<div id="login_container">
+		<div class="panel panel-primary panel_login">
+			<div class="panel-heading">
+				<div class="panel-title"><center>Admin Panel</center></div>
+			</div>
+			<div class="panel-body">
+				<form id="formLogin" action="index.php" method="post">
+					<div class="form-group">
+						<label for="txtUser" class="control-label">Username</label>
+						<input type="text" name="txtUser" id="txtUser" value="" class="form-control">
+					</div>
+					<div class="form-group">
+						<label for="txtPass" class="control-label">Password</label>
+						<input type="password" name="txtPass" id="txtPass" value="" class="form-control">
+					</div>
+					<div class="form-group">
+						<center><button type="submit" name="loginButton" class="btn btn-primary">Login</button></center>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+<?php
+		}
+
+		public static function display_admin_navbar() {
+?>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" style="background-color: black;" href="index.php">Admin Panel</a>
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="https://rizon.net/">Rizon Home</a></li>
+					<li><a href="../index.php">Trivia Reporter</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li ><a href="?action=logout">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+<?php
+		}
 	}
 ?>
